82ad2b17522685a9e19867e57b7e7ba1f6201497
IlyaVasiliev <33105425+IlyaVasiliev@users.noreply.github.com>
2019-06-13 21:19:42 +0300

    Camera movement + fixes

--- Assets/Scripts/Visual/CameraMovement.cs
+++ Assets/Scripts/Visual/CameraMovement.cs
@@ -4,15 +4,82 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] List<BoxCollider> limitColliderList;
+    [SerializeField] float moveSpeed;
+    [SerializeField] LayerMask layerMaskForBaseCollider;
+    [SerializeField] LayerMask layerMaskForLimits;
+
+    [SerializeField] Collider movementBase;
+    [SerializeField] Camera currentCamera;
+    bool lockMovement = false;
+
+    public void Init(Camera cam, Collider baseMovementPlane = null)
     {
-        
+        if (cam != null)
+            currentCamera = cam;
+        currentCamera = cam;
+        if (baseMovementPlane != null)
+            movementBase = baseMovementPlane;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetLock(bool isLocked)
     {
-        
+        lockMovement = isLocked;
     }
+
+    void LateUpdate()
+    {
+        MoveUpdate();
+    }
+
+
+    Vector3 currentMovementVector;
+    Vector3 prevScreenPointOfRaycast;
+    RaycastHit hit;
+    RaycastHit hit2;
+    bool movePerformed = false;
+
+    void MoveUpdate()
+    {
+        if (!lockMovement) // movement screen space
+            if (Input.GetMouseButton(0))
+            {
+                if (Physics.Raycast(currentCamera.ScreenPointToRay(Input.mousePosition), out hit, 3000, layerMaskForBaseCollider.value))
+                {
+                    Physics.Raycast(currentCamera.ScreenPointToRay(prevScreenPointOfRaycast), out hit2, 3000, layerMaskForBaseCollider.value);
+                    currentMovementVector = hit.point - hit2.point;
+                    prevScreenPointOfRaycast = Input.mousePosition;
+
+                    if (!movePerformed)
+                    {
+                        movePerformed = true;
+                    }
+                    else
+                    {
+                        Collider[] limitHits = Physics.OverlapSphere(currentCamera.transform.position - currentMovementVector, 0.5f, layerMaskForLimits.value);
+                        if (limitHits.Length == 0)
+                        {
+                            currentCamera.transform.position = currentCamera.transform.position - currentMovementVector;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                movePerformed = false;
+                prevScreenPointOfRaycast = Vector3.zero;
+            }
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
