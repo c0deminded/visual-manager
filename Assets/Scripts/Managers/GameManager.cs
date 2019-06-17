cd3915b49c801360482f491db39ce7deae443be0
IlyaVasiliev <33105425+IlyaVasiliev@users.noreply.github.com>
2019-06-18 01:55:21 +0300

    Scene changes

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
+        Application.targetFrameRate = 60;
         Instance = this;
         loadingIndicator.DisableLoadingIndicator();
     }
