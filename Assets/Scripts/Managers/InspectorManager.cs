b4254a4a3b47ecf7a3b4e2e652edae2f05517d68
Vladislav <smirvla9@yandex.ru>
2019-06-17 18:24:34 +0300

    inspector basis

---
+++ Assets/Scripts/Managers/InspectorManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InspectorManager : MonoBehaviour
+{
+
+    public Text unitName;
+    public Text unitDesc;
+
+    public static InspectorManager Instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Show(BusinessUnit unit)
+    {
+        unitName.text = unit.naming;
+        unitDesc.text = unit.desc;
+    }
+}
---
+++ Assets/Scripts/Objects/MockUnit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Just fucking stub
+//Do not use to complex purposes!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+public class MockUnit : BusinessUnit
+{
+    void OnMouseDown()
+    {
+        Select();
+        Debug.Log("Hit Mock Unit");
+    }
+}
