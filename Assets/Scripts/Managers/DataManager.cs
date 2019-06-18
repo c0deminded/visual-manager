73b1e1232393aad48f8d445835a5ad38d662f432
Vladislav <smirvla9@yandex.ru>
2019-06-19 02:28:01 +0300

    drivers mock data

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -8,6 +8,28 @@ public class DataManager : MonoBehaviour
     public Color activeButtonColor;
     public Color[] dataColors;
     public Color[] dataColors2;
-    public string [] dataValuesDescription;
+    public string[] dataValuesDescription;
 
+    public static string[] randomNames = 
+        { "Кирилл Петрович",
+        "Иван Иванович",
+        "Петр Кузьмич",
+        "Валерий Леонович",
+        "Александр Витальевич",
+        "Владимир Борисович"};
+
+    public static string[] driversJobs =
+        {"Перевозит ценные документы",
+        "Поставляет продукцию со склада",
+        "Поставляет продукцию с завода",
+            "Инкассация"};
+
+    public string GetRandomName()
+    {
+        return randomNames[Random.Range(0, randomNames.Length-1)];
+    }
+    public string GetDriverDesc()
+    {
+        return driversJobs[Random.Range(0, driversJobs.Length - 1)];
+    }
 }
--- Assets/Scripts/Objects/DeliveryCar.cs
+++ Assets/Scripts/Objects/DeliveryCar.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class DeliveryCar : MonoBehaviour
+public class DeliveryCar : BusinessUnit,IClickable
 {
     [HideInInspector] public Transform[] targets;
     float stoppingDistance = 1f;
     NavMeshAgent agent;
     int currentTargetId = 0;
+
+    public void OnClick()
+    {
+        Select();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         InvokeRepeating("UpdateTargets",0f,3f);
+        naming = GameManager.Instance.datManager.GetRandomName();
+        desc = GameManager.Instance.datManager.GetDriverDesc();
     }
 
     // Update is called once per frame
     void UpdateTargets()
     {
             float currentDist = Vector3.Distance(transform.position, targets[currentTargetId].position);
-            //Debug.Log(currentDist + "//" +  stoppingDistance);
-            //Debug.Log(currentDist <= stoppingDistance && agent.destination != null);
             if (currentDist <= stoppingDistance && agent.destination != null)
             {
                 currentTargetId = (currentTargetId + 1) % targets.Length;
