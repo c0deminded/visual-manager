825785df06d29b73091f6082b98b79105f284d5b
Vladislav <smirvla9@yandex.ru>
2019-06-18 21:31:53 +0300

    delivery cars aka scm vis

---
+++ Assets/Scripts/Managers/DeliveryManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryManager : MonoBehaviour
+{
+    public Route[] routes;
+    int spawnWaves = 0;
+    bool shouldStop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < routes.Length; i++)
+        {
+            spawnWaves = routes[i].carCount > spawnWaves ? routes[i].carCount : spawnWaves;
+        }
+        InvokeRepeating("InitDeliveryCar", 0f, 5f);
+    }
+
+    // Update is called once per frame
+    void InitDeliveryCar()
+    {
+        if (spawnWaves == 0) CancelInvoke();
+        for (int i = 0; i < routes.Length; i++)
+        {
+            if (routes[i].carCount >= spawnWaves)
+            {
+                shouldStop = false;
+                GameObject car = Instantiate(routes[i].carPrefab, routes[i].wayPoints[0].position, Quaternion.identity);
+                car.GetComponent<DeliveryCar>().targets = routes[i].wayPoints;
+                spawnWaves--;
+            }
+        }
+    }
+}
+[System.Serializable]
+public class Route
+{
+    public GameObject carPrefab;
+    public int carCount;
+    public Transform[] wayPoints;
+}
---
+++ Assets/Scripts/Objects/DeliveryCar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DeliveryCar : MonoBehaviour
+{
+    [HideInInspector] public Transform[] targets;
+    float stoppingDistance = 0.5f;
+    NavMeshAgent agent;
+    int currentTargetId = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        InvokeRepeating("UpdateTargets", 0f, 3f);
+    }
+
+    // Update is called once per frame
+    void UpdateTargets()
+    {
+        float currentDist = Vector3.Distance(transform.position, targets[currentTargetId].position);
+        Debug.Log(currentDist + "//" +  stoppingDistance);
+        Debug.Log(currentDist <= stoppingDistance && agent.destination != null);
+        if (currentDist <= stoppingDistance && agent.destination != null)
+        {
+            currentTargetId = (currentTargetId+1)%3;
+            agent.destination = targets[currentTargetId].position;
+        }
+        else
+        {
+            agent.destination = targets[currentTargetId].position;
+        }
+    }
+}
